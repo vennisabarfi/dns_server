0cb711159aa28e81f306ef119123205a15d7b3c8
unknown <vennisabarfi@gmail.com>
2025-02-21 20:23:47 -0500

    Set up server and health endpoint. off to dns config

--- main.go
+++ main.go
@@ -11,9 +11,17 @@ func main() {
 
 	r := mux.NewRouter()
 
+	r.HandleFunc("/", HealthServer)
+
 	err := http.ListenAndServe(":3000", r)
 	if err != nil {
 		log.Println("Error starting server", err)
 	}
+
+}
+
+func HealthServer(w http.ResponseWriter, r *http.Request) {
+	log.Println("Server is healthy")
+	w.Write([]byte("Server is healthy"))
 
 }
