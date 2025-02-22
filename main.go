470eb3d08ac720eefac512cc354d3f89dc12d9fb
unknown <vennisabarfi@gmail.com>
2025-02-21 22:35:22 -0500

    setting up udp server and then testing with dig to google.com to get a good packet to resolve

--- main.go
+++ main.go
@@ -1,22 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	r := mux.NewRouter()
+	port := ":1053"
 
-	r.HandleFunc("/", HealthServer)
-
-	err := http.ListenAndServe(":3000", r)
+	udpAddr, err := net.ResolveUDPAddr("udp", port)
 	if err != nil {
-		log.Println("Error starting server", err)
+		log.Println("Error resolving udp server to port ", err)
 	}
+	fmt.Println(udpAddr)
+
+	// start listening for udp packages on the address
+	conn, err := net.ListenPacket("udp", port)
+	if err != nil {
+		log.Println("Error setting up udp connection ", err)
+	}
+
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 1024)
+		n, addr, err := conn.ReadFrom(buf)
+		if err != nil {
+			log.Println("Error buffering data from udp server", err)
+			continue
+		}
+
+		go serve(conn, addr, buf[:n])
+
+		log.Println("Server buffering data")
+	}
+
+}
+
+func serve(conn net.PacketConn, addr net.Addr, buf []byte) {
+	// 0 - 1: ID
+	// 2: QR(1): Opcode(4)
+	buf[2] |= 0x80 // Set QR bit
+
+	conn.WriteTo(buf, addr)
 
 }
 
